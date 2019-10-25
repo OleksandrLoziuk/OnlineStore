0542127704abe86500ab53f624d267d541ebbd04
AlexanderLoziuk <lozyuk.alexandr@gmail.com>
2019-10-25 23:16:13 +0300

    Initial commit

---
+++ OnlineStore.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.API.Models;
+
+namespace OnlineStore.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+        
+        public DbSet<Value> Values { get; set; }    
+        
+    }
+}
