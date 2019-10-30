b13279a26479b29ff19bbf0f5701af290fdd7825
AlexanderLoziuk <lozyuk.alexandr@gmail.com>
2019-10-30 20:43:34 +0200

    alertify and decoded token in SPA

--- OnlineStore-SPA/src/app/app.module.ts
+++ OnlineStore-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ClickService } from './_services/click.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -25,12 +27,14 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
       ClickService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider, 
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
--- OnlineStore-SPA/src/app/nav/nav.component.ts
+++ OnlineStore-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { ClickService } from '../_services/click.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -12,27 +13,27 @@ export class NavComponent implements OnInit {
   model: any ={};
   registerMode: boolean = false;  
   
-  constructor(private authService: AuthService, private clickService: ClickService) { }
+  constructor(public authService: AuthService, private clickService: ClickService,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in succesfully');
+      this.alertify.success('Logged in succesfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     })
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout(){
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
   registerClick(){
